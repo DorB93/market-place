14d158bf15ad204de3ed8479368c43f1d5287756
Dor Bezalel <dor.netcraft@gmail.com>
2022-11-22 13:18:12 +0200

    Add Product model

---
+++ server/Models/productModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "A product musd have a name"],
+			trim: true,
+			maxlength: [
+				50,
+				"A product name must have less or equal then 40 characters",
+			],
+			minlength: [
+				8,
+				"A product name must have more or equal then 40 characters",
+			],
+		},
+		quantity: {
+			type: Number,
+			required: [true, "A product musd have a quantity"],
+		},
+		ratingsQuantity: {
+			type: Number,
+			default: 0,
+		},
+		price: {
+			type: Number,
+			required: [true, "A tour must have a price"],
+		},
+		summary: {
+			type: String,
+			trim: true,
+			required: [true, "A tour must have a summary"],
+		},
+		description: {
+			type: String,
+			trim: true,
+		},
+		images: [String],
+		seller: {
+			type: mongoose.Schema.ObjectId,
+			ref: "User",
+		},
+	},
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
+productSchema.index({ price: 1, ratingsAverage: -1 });
+productSchema.index({ seller: 1 });
+
+const Product = mongoose.model("Product", userSchema);
+
+module.exports = Product;
