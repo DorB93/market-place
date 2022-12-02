a45d52b7d4bbff1c95677f89c8a1b02a98107c24
Dor Bezalel <dor.netcraft@gmail.com>
2022-12-02 14:31:40 +0200

    add mailtrap email

---
+++ server/utils/email.js
@@ -0,0 +1,30 @@
+require("dotenv").config({ path: "./config.env", override: true });
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+	try {
+		// Create transporter
+
+		const transporter = nodemailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT * 1,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD,
+			},
+		});
+		// console.log(transporter);
+		//Define the email options
+		const mailOptions = {
+			from: "Dor Bezalel <dor.netcraft@gmail.com>",
+			to: options.email,
+			subject: options.subject,
+			text: options.message,
+		};
+		// console.log(mailOptions);
+		//Actually send email
+		await transporter.sendMail(mailOptions);
+	} catch (error) {}
+};
+
+module.exports = sendEmail;
