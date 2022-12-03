a3e57456644d6f7d3791b3edcfecc441b3e523e7
Dor Bezalel <dor.netcraft@gmail.com>
2022-12-03 12:44:08 +0200

    Implement reviewController

---
+++ server/Controllers/reviewController.js
@@ -0,0 +1,24 @@
+const Review = require('./../models/reviewModel');
+const factory = require('./handlerFactory');
+
+function setTourUserIds(req, res, next) {
+  // allow Nested routes
+  if (!req.body.product) req.body.product = req.params.productId;
+  if (!req.body.user) req.body.user = req.user._id;
+  next();
+}
+
+const getAllReviews = factory.getAll(Review);
+const getReview = factory.getOne(Review);
+const createReview = factory.createOne(Review);
+const updateReview = factory.updateOne(Review);
+const deleteReview = factory.deleteOne(Review);
+
+module.exports = {
+  setTourUserIds,
+  getReview,
+  createReview,
+  updateReview,
+  getAllReviews,
+  deleteReview,
+};
