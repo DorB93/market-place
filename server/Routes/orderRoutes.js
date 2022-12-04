efae2b722825a8bf9bf996baf4c88a10da91b716
Dor Bezalel <dor.netcraft@gmail.com>
2022-12-04 19:30:00 +0200

    Implement orders routes

---
+++ server/Routes/orderRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const orderController = require("../controllers/orderController");
+const authController = require("../controllers/authController");
+
+const router = express.Router({ mergeParams: true });
+
+router.use(authController.protect);
+
+router.get("/my-customers-orders", orderController, getAllSellerOrders);
+
+router
+	.route("/")
+	.get(authController.restrictTo("admin"), orderController.getAllOrders)
+	.post(authController.restrictTo("user"), orderController.createOrder);
+
+router
+	.route("/:id")
+	.get(orderController.getOrder)
+	.patch(
+		authController.restrictTo("admin", "user"),
+		orderController.updateOrder
+	)
+	.delete(
+		authController.restrictTo("admin", "user"),
+		orderController.deleteOrder
+	);
+
+module.exports = router;
