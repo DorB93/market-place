4055df183d9c693b1135e7d9fdf7338355d232fb
Dor Bezalel <dor.netcraft@gmail.com>
2022-12-09 12:08:54 +0200

    Implement ProductDetails page

--- client/src/pages/ProductDetail.js
+++ client/src/pages/ProductDetail.js
@@ -1,7 +1,78 @@
 import React from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import useFetch from "./../hooks/useFetch";
+import useProducts from "../hooks/useProducts";
+import Product, { BtnAddToCart } from "../components/Product";
+
+const ProductWrapper = styled.div`
+	width: 80%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-around;
+	gap: 15px;
+`;
+const Details = styled.section`
+	width: 80%;
+	display: flex;
+	justify-content: space-between;
+	gap: 50px;
+`;
+const ProductImg = styled.img`
+	min-width: 250px;
+	object-fit: contain;
+	max-height: 500px;
+`;
+const TextDetails = styled.div`
+	width: 50%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+`;
+
+const RelatedContainer = styled.section`
+	display: flex;
+	width: 90%;
+	flex-wrap: wrap;
+	overflow: auto;
+	justify-content: center;
+`;
+
+const PriceAction = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
 
 function ProductDetail() {
-	return <div>ProductDetail</div>;
+	const { id } = useParams();
+	const productData = useFetch(`https://fakestoreapi.com/products/${id}`);
+	const sameCategory = useProducts()
+		.filter(
+			(p) => p.id !== productData.id && p.category === productData.category
+		)
+		.map((p) => <Product key={p.id} product={p} />);
+	return (
+		<>
+			<ProductWrapper>
+				<h1>{productData.title}</h1>
+				<Details>
+					<ProductImg src={productData.image} alt={productData.title} />
+					<TextDetails>
+						<h3>Category: {productData.category}</h3>
+						<p>{productData.description}</p>
+						<PriceAction>
+							<span>Price: ${Number(productData.price).toFixed(2)}</span>
+							<BtnAddToCart>Add to cart</BtnAddToCart>
+						</PriceAction>
+					</TextDetails>
+				</Details>
+				<h3>More from this category:</h3>
+				<RelatedContainer>{sameCategory}</RelatedContainer>
+			</ProductWrapper>
+		</>
+	);
 }
 
 export default ProductDetail;
