41c4e6fda3aad21a9a0df30cba04aefb8e49188e
Dor Bezalel <dor.netcraft@gmail.com>
2022-12-21 23:05:03 +0200

    Remove AuthProvider

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "../../client/src/reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+import { AuthProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<AuthProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</AuthProvider>
 	</React.StrictMode>
 );
 
