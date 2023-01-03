a9f1e5a2b93b716846fa3329612c7d0cea181cdb
Dor Bezalel <dor.netcraft@gmail.com>
2023-01-03 16:50:34 +0200

    Dashboard initial

--- client/src/components/Seller/SellerDashboard.js
+++ client/src/components/Seller/SellerDashboard.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import myAxios from "../../api";
+import LoadingSpinner from "../LoadingSpinner";
 
+// async function getInvoices() {
+// 	const res = await myAxios.get("invoices/my-invoices");
+// 	return res;
+// }
+const DashboardContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
 function SellerDashboard() {
-	return <div>dashboard here</div>;
+	// const [invoices, setInvoices] = useState([]);
+	// const [isLoading, setIsLoading] = useState(true);
+
+	// useEffect(() => {
+	// 	setIsLoading(true);
+	// 	getInvoices().then((res) => {
+	// 		console.log(res);
+	// 		setInvoices(res);
+	// 		setIsLoading(false);
+	// 	});
+	// }, []);
+	return (
+		<DashboardContainer>
+			{/* {isLoading ? (
+				<LoadingSpinner />
+			) : (
+				<> */}
+			<h1>Sorry... Not Implement yet.... </h1>
+			{/* </>
+			)} */}
+		</DashboardContainer>
+	);
 }
 
 export default SellerDashboard;
